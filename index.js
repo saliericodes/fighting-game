8c8acdb32f37295e8e49907d1efba13af282b80e
Raphael Melo <34170746+dev-salieri@users.noreply.github.com>
2022-12-15 15:16:50 -0300

    add player

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ canvas.width = 1024;
 canvas.height = 512;
 
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+class Sprite {
+    constructor(position) {
+        this.position = position;
+    }
+
+    draw() {
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.position.x, this.position.y, 50, 150);
+    }
+}
+
+const player = new Sprite({
+    x: 0,
+    y: 0
+})
+
+player.draw();
